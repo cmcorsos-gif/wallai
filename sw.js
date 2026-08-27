ab5ac2abe409e53ba1606ba3328de874c6b26baf
cmcorsos-gif <cmcorsos@gmail.com>
2026-08-27 10:42:47 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'wallai-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// Network-first: always try to fetch the latest version first (so updates show up
+// immediately), and only fall back to the cached copy when there's no connection.
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
